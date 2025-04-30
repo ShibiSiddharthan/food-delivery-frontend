183d7ebd5b3b8181ff3656167cfd1471b5194854
ShibiSiddharthan <shibisiddharthan@gmail.com>
2025-04-30 21:12:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import Home from "./Pages/Home/Home";
 import Cart from "./Pages/Cart/Cart";
 import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
 import Footer from "./Components/Footer/Footer";
-import LoginPopUp from "./Components/LoginPopup/LoginPopUp";
+import LoginPopup from "./Components/LoginPopup/LoginPopup";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
-      {showLogin ? <LoginPopUp setShowLogin={setShowLogin}/>   : <></>}
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin}/>   : <></>}
       <div className="app">
         <Navbar  setShowLogin={setShowLogin}/>
         <Routes>
